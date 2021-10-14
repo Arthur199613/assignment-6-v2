39dc0d27b2ed88c46e826e2cf45d8c04ebd53c9d
Arthur <kamarouski-a@ulster.ac.uk>
2021-10-14 15:13:20 +0100

    ...

---
+++ SpringMVC/src/main/java/com/example/SpringMVC/GreetingsController.java
@@ -0,0 +1,19 @@
+package com.example.SpringMVC;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class GreetingsController
+
+{
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(name = "name",required = false, defaultValue = "world")
+                           String name, Model model){
+        model.addAttribute("name",name);
+        return "greeting";
+
+    }
+}
